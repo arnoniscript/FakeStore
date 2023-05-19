f93d16dfef65519d3a0fbb8d01b70b9faa0f2132
Luiz Arnoni Fraga <luiz.arnoni@seguranca.mg.gov.br>
2023-05-19 20:46:42 -0300

    Criação do componente Bestselling sem refinamento

--- src/components/Bestselling/Bestselling.jsx
+++ src/components/Bestselling/Bestselling.jsx
@@ -1,19 +1,30 @@
 import './styles.css'
+import { BsArrowRight } from 'react-icons/bs';
 
 const Bestselling = () => {
     return( 
-      <body className='containerbody'>      <div className='Bestselling'>
-Best Selling Plants
-      </div>
-      <div className='Showitem'>
+      <body className='containerbody'>      <div className='Bestselling'> <p>
+Best Selling Plants</p>
 
+<div className="description"> <p>Easiest way to healthy life by buying your favorite plants </p></div>
+      <button id="mainbutton">See more <BsArrowRight /></button>
       </div>
-      <div className='Showitem'>
-
-      </div>
-      <div className='Showitem'>
-
-      </div>
+      <figure className='Showitem'>
+        <img src='03.jpg'></img>
+<div className='itemname'>Plantinha 01</div>
+<div className='itemprice'>R$948,00</div>
+      </figure>
+      <figure className='Showitem'>
+        <img src='03.jpg'></img>
+<div className='itemname'>Plantinha 02</div>
+<div className='itemprice'>R$91,00</div>
+      </figure>
+      <figure className='Showitem'>
+        <img src='03.jpg'></img>
+<div className='itemname'>Plantinha 03</div>
+<div className='itemprice'>R$9448,00</div>
+      </figure>
+      
       </body>
 
       
