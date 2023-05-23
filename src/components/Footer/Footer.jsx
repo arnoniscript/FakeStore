e2508de538594e752887d9b481d2a52907129481
Luiz Arnoni Fraga <luiz.arnoni@seguranca.mg.gov.br>
2023-05-23 17:54:38 -0300

    Início do footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./styles.css"
+
+const Footer = () => {
+    return(
+        <div className="footer">
+           
+ <div id="logo">GREENMIND</div>
+ 
+
+        </div>
+    )
+}
+
+export default Footer
