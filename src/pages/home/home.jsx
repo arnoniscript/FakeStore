efd4c440ca813982d62409e73c1373753d84ea04
Luiz Arnoni Fraga <luiz.arnoni@seguranca.mg.gov.br>
2023-05-25 14:07:04 -0300

    + Footer, + rota Home, +conexão com axios

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,16 @@
+import { Bestselling, Aboutus, HomeCategories } from '../../components'
+
+
+const Home = () => {
+  return(
+    <main>
+  
+      <Bestselling />
+      <Aboutus />
+      <HomeCategories />
+   
+    </main>
+  )
+}
+
+export default Home
